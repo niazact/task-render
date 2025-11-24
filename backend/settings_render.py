2bbb5f96b6782e843b4da697c7a882f42d738f68
Niaz Mohammad <niaz.act@gmail.com>
2025-11-24 13:08:06 +0530

    new changes3 to settings.py

--- backend/settings_render.py
+++ backend/settings_render.py
@@ -2,10 +2,10 @@ import os
 import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
-ALLOWED_HOSTS =[os.environ.get['RENDER_EXTERNAL_HOSTNAME'], 'localhost', ]
-CSRF_TRUSTED_ORIGINS=['https://' + os.environ.get['RENDER_EXTERNAL_HOSTNAME']] 
+ALLOWED_HOSTS = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+CSRF_TRUSTED_ORIGINS= 'https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME') 
 DEBUG =False
-SECRET_KEY = os.environ.get['SECRET_KEY']  
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
