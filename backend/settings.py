db4be46ce5ae958506c9e7354349e9eaacfb77d8
Niaz Mohammad <niaz.act@gmail.com>
2025-11-24 12:28:41 +0530

    new changes first commit render

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*4f$2&i5k!casxuzztu+!phg3sd&&!+n@_nz(l1r#wy69oegw!'
+#SECRET_KEY = 'django-insecure-*4f$2&i5k!casxuzztu+!phg3sd&&!+n@_nz(l1r#wy69oegw!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
